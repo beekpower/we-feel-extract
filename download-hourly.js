0cf6b5913f6d67cbe1c150b9fe49df0d6fae0ca6
nicholas <nicholas@pop-os.localdomain>
2020-06-21 22:24:59 -0400

    Download raw data to JSON files

---
+++ download-hourly.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const moment = require('moment');
+const fs = require('fs');
+
+// Change these for download date range;
+const START_DATE = '01-01-2020';
+const END_DATE = '31-05-2020';
+const GRANULARITY = 'hour';
+
+const filePath = './output/raw/' + Date.now() + '|' + START_DATE + '|' + END_DATE + '|' + GRANULARITY + '.json';
+const startDate = moment(START_DATE, 'DD-MM-YYYY');
+const endDate = moment(END_DATE, 'DD-MM-YYYY');
+
+(async () => {
+  const response = await axios.get('http://wefeel.csiro.au/api/emotions/primary/timepoints', {
+    params: {
+      granularity: GRANULARITY,
+      start: startDate.valueOf(),
+      end: endDate.valueOf(),
+      // continent: 'northAmerica'
+    }
+  });
+  const dates = response.data;
+  fs.writeFileSync(filePath, JSON.stringify(dates, null, 2));
+})();
